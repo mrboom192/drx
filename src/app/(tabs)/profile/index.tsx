087bf0a402be10fc1def42cc76ca248febf57172
mrboom192 <mrboom192@gmail.com>
2025-04-04 02:37:51 -0500

    Add profile screen links placeholders

--- src/app/(tabs)/profile/index.tsx
+++ src/app/(tabs)/profile/index.tsx
@@ -1,19 +1,45 @@
-import { View, StyleSheet, Text, SafeAreaView, Image } from "react-native";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
 import React, { useMemo } from "react";
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { useThemedStyles } from "@/hooks/useThemeStyles";
 import userData from "@/../assets/data/user.json";
 import { User } from "@/types/user";
 import Colors from "@/constants/Colors";
+import Avatar from "@/components/Avatar";
+import { Ionicons } from "@expo/vector-icons";
+import { useSession } from "@/contexts/AuthContext";
+
+const items = [
+  {
+    icon: "person-outline",
+    label: "Manage your personal information",
+  },
+  {
+    icon: "medkit-outline",
+    label: "View your medical records",
+  },
+  {
+    icon: "cloud-upload-outline",
+    label: "Manage uploaded images and files",
+  },
+  {
+    icon: "call-outline",
+    label: "Contact customer support",
+  },
+  {
+    icon: "notifications-outline",
+    label: "Notifications",
+  },
+  {
+    icon: "shield-checkmark-outline",
+    label: "View privacy policy and terms of service",
+  },
+];
 
 const Profile = () => {
   const user = useMemo(() => userData as User, []);
-  const {
-    colorScheme,
-    themeBorderStyle,
-    themeTextStylePrimary,
-    themeTextStyleSecondary,
-  } = useThemedStyles();
+  const { colorScheme } = useThemedStyles();
+  const { signOut } = useSession();
 
   return (
     <SafeAreaView
@@ -35,175 +61,121 @@ const Profile = () => {
           flex: 1,
           flexDirection: "column",
           justifyContent: "flex-start",
+          alignItems: "center",
           marginHorizontal: 16,
         }}
       >
+        {/* User Card */}
         <View
           style={{
-            width: 100,
-            height: 100,
-            backgroundColor: "#FF00FF",
-            marginBottom: 50,
+            width: "100%",
+            backgroundColor: "#FFF",
+            marginBottom: 16,
+
+            paddingVertical: 32,
+            flexDirection: "row",
+            gap: 16,
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 16,
+
+            shadowColor: "#000", // ios
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.5,
+            shadowRadius: 3.84,
+            elevation: 10, // android
           }}
-        ></View>
-        <View
-          style={[
-            themeBorderStyle,
-            { flexDirection: "column", padding: 16, gap: 16, borderRadius: 16 },
-          ]}
         >
-          {/* User name and role */}
-          <View style={{ flexDirection: "row", alignItems: "center", gap: 16 }}>
-            <Image source={{ uri: user.profileImage }} style={styles.image} />
-            <View style={{ flexDirection: "column" }}>
-              <Text
-                style={[
-                  themeTextStylePrimary,
-                  { fontFamily: "dm-sb", fontSize: 20 },
-                ]}
-              >
-                {user.name}
-              </Text>
-              <Text
-                style={{
-                  fontFamily: "dm-sb",
-                  fontSize: 20,
-                  color: Colors.primary,
-                }}
-              >
-                {user.role}
-              </Text>
-            </View>
-          </View>
-
-          {/* Divider */}
-          <View
-            style={[themeBorderStyle, { borderWidth: 0, borderBottomWidth: 1 }]}
-          />
-
-          {/* Metrics */}
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            {/* Gender */}
-            <View>
-              <Text
-                style={[
-                  themeTextStyleSecondary,
-                  { fontFamily: "dm", fontSize: 14 },
-                ]}
-              >
-                Gender
-              </Text>
-              <Text
-                style={[
-                  themeTextStylePrimary,
-                  { fontFamily: "dm-sb", fontSize: 20 },
-                ]}
-              >
-                {user.gender}
-              </Text>
-            </View>
-            {/* Age */}
-            <View>
-              <Text
-                style={[
-                  themeTextStyleSecondary,
-                  { fontFamily: "dm", fontSize: 14 },
-                ]}
-              >
-                Age
-              </Text>
-              <Text
-                style={[
-                  themeTextStylePrimary,
-                  { fontFamily: "dm-sb", fontSize: 20 },
-                ]}
-              >
-                {user.age}
-              </Text>
-            </View>
-
-            {/* Weight */}
-            <View>
-              <Text
-                style={[
-                  themeTextStyleSecondary,
-                  { fontFamily: "dm", fontSize: 14 },
-                ]}
-              >
-                Weight
-              </Text>
-              <Text
-                style={[
-                  themeTextStylePrimary,
-                  { fontFamily: "dm-sb", fontSize: 20 },
-                ]}
-              >
-                {user.weight.value} {user.weight.unit}
-              </Text>
-            </View>
-
-            {/* Height */}
-            <View>
-              <Text
-                style={[
-                  themeTextStyleSecondary,
-                  { fontFamily: "dm", fontSize: 14 },
-                ]}
-              >
-                Height
-              </Text>
-              <Text
-                style={[
-                  themeTextStylePrimary,
-                  { fontFamily: "dm-sb", fontSize: 20 },
-                ]}
-              >
-                {user.height.value} {user.height.unit}
-              </Text>
-            </View>
+          <Avatar size={64} initials={`${user.name[0]}`} />
+          <View>
+            <Text style={{ fontFamily: "dm-sb", fontSize: 20 }}>
+              {user.name}
+            </Text>
+            <Text
+              style={{ fontFamily: "dm", fontSize: 16, color: Colors.primary }}
+            >
+              {user.role ? user.role : "Patient"}
+            </Text>
           </View>
         </View>
+
+        {/* Main Message */}
+        <Text style={{ fontFamily: "dm-sb", fontSize: 20, marginBottom: 16 }}>
+          How can we help you, Trevor?
+        </Text>
+
+        {/* Links */}
+        <View style={{ width: "100%" }}>
+          {/* Create rows of two */}
+          {Array.from({ length: 3 }).map((_, rowIdx) => (
+            <View
+              key={rowIdx}
+              style={{
+                flexDirection: "row",
+                gap: 16,
+                justifyContent: "space-between",
+                marginBottom: 16,
+              }}
+            >
+              {items.slice(rowIdx * 2, rowIdx * 2 + 2).map((item, idx) => (
+                // The links themselves
+                <Link key={idx} href={"/(tabs)/profile"} asChild>
+                  <TouchableOpacity
+                    style={{
+                      flex: 1,
+                      padding: 16,
+                      flexDirection: "column",
+                      justifyContent: "space-between",
+                      gap: 16,
+                      borderRadius: 12,
+                      borderWidth: 1,
+                      borderColor: Colors.light.faintGrey,
+                    }}
+                  >
+                    <Ionicons
+                      name={item.icon as keyof typeof Ionicons.glyphMap}
+                      size={24}
+                      color={Colors.primary}
+                      style={{ marginBottom: 12 }}
+                    />
+                    <Text style={{ fontSize: 12, color: Colors.light.grey }}>
+                      {item.label}
+                    </Text>
+                  </TouchableOpacity>
+                </Link>
+              ))}
+            </View>
+          ))}
+        </View>
+
+        {/* Logout button */}
+        <TouchableOpacity
+          onPress={signOut}
+          style={{
+            marginTop: 24,
+            paddingVertical: 12,
+            paddingHorizontal: 32,
+            backgroundColor: Colors.primary,
+            borderRadius: 8,
+          }}
+        >
+          <Text
+            style={{
+              color: "#FFF",
+              fontSize: 16,
+              fontFamily: "dm-sb",
+              textAlign: "center",
+            }}
+          >
+            Log out
+          </Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  actionRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 24,
-    marginBottom: 16,
-    gap: 16,
-  },
-  filterBtn: {
-    height: 56,
-    width: 56,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 9999,
-  },
-  searchBtn: {
-    flexDirection: "row",
-    alignItems: "center",
-    height: 56,
-    gap: 10,
-    flex: 1,
-    padding: 14,
-    borderRadius: 30,
-  },
-  image: {
-    width: 64,
-    height: 64,
-    borderRadius: 9999,
-  },
-});
 
 export default Profile;

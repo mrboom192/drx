c3cece80251b946b75010cfefe2470d63b0ded66
mrboom192 <mrboom192@gmail.com>
2025-05-10 06:19:28 -0500

    Add some notification preliminary stuff

--- src/components/DoctorHomeHeader.tsx
+++ src/components/DoctorHomeHeader.tsx
@@ -6,6 +6,9 @@ import UserRow from "./UserRow";
 const DoctorHomeHeader = () => {
   const insets = useSafeAreaInsets();
 
+  // Add a notifications section
+  // 2 Notifcations are added when a new account is created: verify license and create public profile for doctor
+
   return (
     <View style={{ backgroundColor: "#FFF", paddingTop: insets.top }}>
       <View

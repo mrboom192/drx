da04b54d558f4141cc1b8fdd320e3a0c61fcb134
mrboom192 <mrboom192@gmail.com>
2025-05-16 01:52:58 -0500

    Update some page layouts

--- src/app/(protected)/doctor/_layout.tsx
+++ src/app/(protected)/doctor/_layout.tsx
@@ -1,33 +1,16 @@
-import { Ionicons } from "@expo/vector-icons";
-import { router, Stack } from "expo-router";
+import PageHeader from "@/components/PageHeader";
+import { Stack } from "expo-router";
 import React from "react";
-import { TouchableOpacity } from "react-native";
 
 const DoctorProfileLayout = () => {
   return (
-    <Stack
-      screenOptions={{
-        title: "Booking",
-        headerTitleStyle: { fontFamily: "dm-sb" },
-        headerTitleAlign: "center",
-        headerShadowVisible: false,
-      }}
-    >
-      <Stack.Screen
-        name="[id]"
-        options={{ title: "Doctor Profile", headerTitleAlign: "center" }}
-      />
+    <Stack screenOptions={{ header: (props) => <PageHeader {...props} /> }}>
+      <Stack.Screen name="[id]" options={{ title: "Doctor Profile" }} />
       <Stack.Screen
         name="booking"
         options={{
           title: "Book Consultation",
-          headerTitleAlign: "center",
           presentation: "modal",
-          headerLeft: () => (
-            <TouchableOpacity onPress={() => router.back()}>
-              <Ionicons name="close" size={24} color="#000" />
-            </TouchableOpacity>
-          ),
         }}
       />
     </Stack>
--- src/app/(protected)/notifications.tsx
+++ src/app/(protected)/notifications.tsx
@@ -1,11 +1,12 @@
+import PageScrollView from "@/components/PageScrollView";
+import { TextRegular } from "@/components/StyledText";
 import React from "react";
-import { Text, View } from "react-native";
 
 const Notifications = () => {
   return (
-    <View>
-      <Text>notifications</Text>
-    </View>
+    <PageScrollView>
+      <TextRegular>notifications</TextRegular>
+    </PageScrollView>
   );
 };
 

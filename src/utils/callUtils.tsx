9aed16e7016b47c33e05a79a0b4013fadf7c4135
Jason Nguyen <mrboom192@gmail.com>
2025-05-27 03:31:43 -0500

    Update calendar styling and code refactor for callid

---
+++ src/utils/callUtils.tsx
@@ -0,0 +1,8 @@
+export const formatTime = (seconds: number) => {
+  const hrs = Math.floor(seconds / 3600).toString();
+  const mins = Math.floor((seconds % 3600) / 60)
+    .toString()
+    .padStart(2, "0");
+  const secs = (seconds % 60).toString().padStart(2, "0");
+  return `${hrs}:${mins}:${secs}`;
+};
